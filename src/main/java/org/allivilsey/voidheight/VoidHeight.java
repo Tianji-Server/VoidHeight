9fa2d75f0384860b7e5e0fba052350bc41e87cf5
Alliversey <126925355+Alliversey@users.noreply.github.com>
2025-01-10 05:52:30 +0800

    Update VoidHeight.java
    添加游戏中修改虚空高度功能

--- src/main/java/org/allivilsey/voidheight/VoidHeight.java
+++ src/main/java/org/allivilsey/voidheight/VoidHeight.java
@@ -1,15 +1,23 @@
 package org.allivilsey.voidheight;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 
-public final class VoidHeight extends JavaPlugin {
+public final class VoidHeight extends JavaPlugin implements CommandExecutor {
 
-    private final static int voidHeight = 0;
+    private static int voidHeight;
 
     @Override
     public void onEnable() {
+
+        saveDefaultConfig();
+
+        getConfig().getDouble("Height", 0);
 
         Bukkit.getScheduler().runTaskTimer(this, () -> {
             for (Player player : Bukkit.getOnlinePlayers()) {
@@ -23,5 +31,32 @@ public final class VoidHeight extends JavaPlugin {
 
     @Override
     public void onDisable() {
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender.isOp() || sender.hasPermission("voidHeight.config")) {
+            if (args.length != 1) {
+                sender.sendMessage("你需要输入你想要设置的高度");
+            } else {
+                try {
+                    voidHeight = Integer.parseInt(args[0]);
+                    getConfig().set("Height", args[0]);
+                    saveConfig();
+                    returnMessage(sender, "虚空高度已变更为" + args[0]);
+                } catch (Exception e) {
+                    returnMessage(sender, Color.RED + "错误：" + e);
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static void returnMessage(CommandSender sender, String message) {
+        if (sender instanceof Player) {
+            sender.sendMessage("[VoidHeight] " + message);
+        } else {
+            Bukkit.getLogger().info("[VoidHeight] " + message);
+        }
     }
 }
